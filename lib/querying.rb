81412d0dd62b2ee6aad871a8d821c78a0d07f39a
Chloe Steffens <caforonda103@yahoo.com>
2020-06-01 16:31:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,14 +31,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title
-  FROM characters
-  INNER JOIN series
-  ON characters.series_id = series.id
-  GROUP BY Series.title, species
-  HAVING characters.species = 'human'
-  ORDER BY COUNT(species) DESC
-  LIMIT 1;"
+  "SELECT series.title FROM series 
+  JOIN characters ON series.id = characters.series_id 
+  WHERE characters.species = 'human' LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
